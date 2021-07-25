2edafb5c0a4443a3f956d3e2eed842e4fcef33a8
Rahul76979 <rahulrsirwani@gmail.com>
2021-07-26 04:39:48 +0530

    changes for build

--- src/main/java/com/catering/ssvc/Controller/CateringMaterialOrderCtl.java
+++ src/main/java/com/catering/ssvc/Controller/CateringMaterialOrderCtl.java
@@ -1,7 +1,6 @@
 package com.catering.ssvc.Controller;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,24 +8,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.catering.ssvc.DTO.CateringMaterialOrderEntity;
-
 @RestController
 @CrossOrigin(value="*")
 @RequestMapping(value="materialOrder")
 public class CateringMaterialOrderCtl {
 
-	@PostMapping("addMaterialOrder")
-	public String addMaterialOrder(@RequestBody CateringMaterialOrderEntity orderObject)
-	{
+//	@PostMapping("addMaterialOrder")
+//	public String addMaterialOrder(@RequestBody CateringMaterialOrderEntity orderObject)
+//	{
 //		try {
 //			JSONObject jsonData = new JSONObject(orderObject);
 //		} catch (JSONException e) {
 //			e.printStackTrace();
 //		}
-		String customerName = orderObject.getCustomerName();
-		return "success";
-	}
+//		String customerName = orderObject.getCustomerName();
+//		return "success";
+//	}
 	
 	@GetMapping("/testJenkins")
 	public String getForJenkins()
