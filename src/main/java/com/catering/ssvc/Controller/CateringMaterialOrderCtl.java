fd2a7d010549f24a5bb291c2a4d2bcb3a13e6f1b
Rahul76979 <rahulrsirwani@gmail.com>
2021-07-26 03:02:07 +0530

    First push to Github

---
+++ src/main/java/com/catering/ssvc/Controller/CateringMaterialOrderCtl.java
@@ -0,0 +1,29 @@
+package com.catering.ssvc.Controller;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.catering.ssvc.DTO.CateringMaterialOrderEntity;
+
+@RestController
+@CrossOrigin(value="*")
+@RequestMapping(value="materialOrder")
+public class CateringMaterialOrderCtl {
+
+	@PostMapping("addMaterialOrder")
+	public String addMaterialOrder(@RequestBody CateringMaterialOrderEntity orderObject)
+	{
+//		try {
+//			JSONObject jsonData = new JSONObject(orderObject);
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+		String customerName = orderObject.getCustomerName();
+		return "success";
+	}
+}
